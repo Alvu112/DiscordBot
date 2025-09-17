2ac0f6e21bbdcb9b55ac86720e4c5b0d2becc826
Alvu <s3tenta@gmail.com>
2025-09-18 01:48:56 +0200

    Create restart.sh

---
+++ restart.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+while true; do
+    if ! pgrep -f TestBot.py > /dev/null; then
+        echo "TestBot.py no está activo. Iniciando..."
+        python TestBot.py 2>&1 &
+    fi
+    sleep 5
+done &
